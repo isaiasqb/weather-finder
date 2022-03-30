212cb832ebc0c3727beb9c3db2bea2f47fea1380
IsaiasQB <isaias.qb@hotmail.com>
2022-03-30 02:36:31 -0400

    added API call for getting daily weather

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,29 +1,48 @@
 var apiKey = "5dacd7c8902f3b17b76a6c25dd7320ee";
-// DOM elements
+    // DOM elements
 var cityFormEl = document.querySelector("#city-form");
 var cityNameEl = document.querySelector("#city-name");
 var cityNameBtnEl = document.querySelector("#city-name-btn")
+var currentStatsEl = document.querySelector("#current-day")
 
 
- //API call function
-var getWeatherInfo = function(cityName){fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey)
+    //API call function for searching by City Name
+var getWeatherInfo = function(cityName){fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&cnt=7&appid="+apiKey)
 
     .then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            //console.log(data);
+
+            console.log("longitude: " + data.coord.lon);
+            var longitude = data.coord.lon;
+            console.log("latitude: " + data.coord.lat);
+            var latitude = data.coord.lat;
+
+            getDailyWeather(latitude, longitude)
         });//end of inner.then
     });//end of .then
+};
+
+
+    // API call for daily weather
+var getDailyWeather = function(lat, lon){
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&exclude=hourly,minutely&units=metric&appid="+apiKey)
+   .then(function(response){
+       response.json().then(function(data){
+           console.log(data);
+       });//end of inner.then
+   });//end of .then
 };
 
 
     //SUBMIT form function
 var cityNameSubmit = function(event) {
     event.preventDefault();
-    console.log(event);
+
         //get the name of the city from the input of the user
     var cityName = cityNameEl.value.trim()
 
-        //if there is a name make the API call
+        //if there is a name make the API call for the weather stats
     if(cityName){
         console.log("searching for the weather in: "+ cityName)
         getWeatherInfo(cityName)
@@ -34,8 +53,9 @@ var cityNameSubmit = function(event) {
 }
 
 
-    //submit form with user input
+    //SUBMIT form with user input
 cityFormEl.addEventListener("submit", cityNameSubmit);
+
 
 
 
