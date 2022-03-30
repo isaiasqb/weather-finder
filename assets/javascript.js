73d45304d6fbc5cef3487d336e989a0479f2fac7
IsaiasQB <isaias.qb@hotmail.com>
2022-03-30 00:14:04 -0400

    added global variables and event listener to form

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,11 @@
 var apiKey = "5dacd7c8902f3b17b76a6c25dd7320ee";
+// DOM elements
+var cityFormEl = document.querySelector("#city-form");
+var cityNameEl = document.querySelector("#city-name");
+var cityNameBtnEl = document.querySelector("#city-name-btn")
 
+
+ //API call function
 var getWeatherInfo = function(){fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="+apiKey)
     .then(function(response){
         response.json().then(function(data){
@@ -8,4 +14,17 @@ var getWeatherInfo = function(){fetch("https://api.openweathermap.org/data/2.5/o
     });//end of .then
 };
 
+
+    //SUBMIT form function
+var cityNameSubmit = function(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+
+    //submit form with user input
+cityFormEl.addEventListener("submit", cityNameSubmit);
+
+
 getWeatherInfo()
+
