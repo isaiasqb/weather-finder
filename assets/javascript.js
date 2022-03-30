05edec5a9535c5af6bd47c4e862805a7c236f397
IsaiasQB <isaias.qb@hotmail.com>
2022-03-30 12:00:45 -0400

    added 5 days weather and created a Date variable

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,23 @@
 var apiKey = "5dacd7c8902f3b17b76a6c25dd7320ee";
+
     // DOM elements
 var cityFormEl = document.querySelector("#city-form");
 var cityNameEl = document.querySelector("#city-name");
 var cityNameBtnEl = document.querySelector("#city-name-btn")
 var currentStatsEl = document.querySelector("#current-day")
+var containerDaysEl = document.querySelector(".future-days");
+
+    //CREATE the date
+var today = new Date()
+var day = today.getDate()
+var month = today.getMonth()+1
+var year = today.getFullYear()
+
+
 
 
     //1st API call Searching the cities longitude and latitude stats
-var getWeatherInfo = function(cityName){fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&cnt=7&appid="+apiKey)
+var getWeatherInfo = function(cityName){fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&cnt=5&appid="+apiKey)
     .then(function(response){
         response.json().then(function(data){
 
@@ -15,6 +25,7 @@ var getWeatherInfo = function(cityName){fetch("https://api.openweathermap.org/da
             var cityTitle = document.createElement("h2");
             cityTitle.innerText = data.name + ", " + data.sys.country;
             currentStatsEl.appendChild(cityTitle);
+
                 //capture the longitude and latitude of the city and send them as parameters
             var longitude = data.coord.lon;
             var latitude = data.coord.lat;
@@ -25,24 +36,43 @@ var getWeatherInfo = function(cityName){fetch("https://api.openweathermap.org/da
 
     // 2nd API call: for daily weather
 var getDailyWeather = function(lat, lon){
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&exclude=hourly,minutely&units=metric&appid="+apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&exclude=hourly,minutely&units=metric&cnt=5&appid="+apiKey)
    .then(function(response){
        response.json().then(function(data){
            console.log(data);
 
                 // create the HTML elements for the current day stats
+            var cityDate = document.createElement("p");
+            cityDate.className = "badge bg-dark";
+            cityDate.innerText = today;
             var cityTemp = document.createElement("p");
             cityTemp.innerHTML = "<span class='alert-dark'>Temprature:</span>"+" "+ data.daily[0].temp.day +"°C";
-
             var cityWind = document.createElement("p");
             cityWind.innerHTML = "<span class='alert-dark'>Wind Speed:</span>"+" "+ data.daily[0].wind_speed +" Km/h";
-
             var cityHumid = document.createElement("p");
             cityHumid.innerHTML = "<span class='alert-dark'>Humidity:</span>"+" "+ data.daily[0].humidity +"%";
-
             var cityUvi = document.createElement("p");
             cityUvi.innerHTML = "<span class='alert-dark'>UVI Index:</span>"+" "+ data.daily[0].uvi;
-            currentStatsEl.append(cityTemp, cityWind, cityHumid, cityUvi);
+            currentStatsEl.append(cityDate, cityTemp, cityWind, cityHumid, cityUvi);
+
+                // dynamically generate 5 day forecast
+            for (var i = 1; i < 6; i++) {
+                    //card element
+                var dayCard = document.createElement("div")
+                    //5 coming days
+                var cityTemp = document.createElement("p");
+                cityTemp.innerHTML = "<span class='alert-dark'>Temprature:</span>"+" "+ data.daily[i].temp.day +"°C";
+                var cityWind = document.createElement("p");
+                cityWind.innerHTML = "<span class='alert-dark'>Wind Speed:</span>"+" "+ data.daily[i].wind_speed +" Km/h";
+                var cityHumid = document.createElement("p");
+                cityHumid.innerHTML = "<span class='alert-dark'>Humidity:</span>"+" "+ data.daily[i].humidity +"%";
+                var cityUvi = document.createElement("p");
+                cityUvi.innerHTML = "<span class='alert-dark'>UVI Index:</span>"+" "+ data.daily[i].uvi;
+                dayCard.append(cityTemp, cityWind, cityHumid, cityUvi);
+
+                    //append all to the document
+                containerDaysEl.append(dayCard);
+            }//end of for loop
 
        });//end of inner.then
    });//end of .then
